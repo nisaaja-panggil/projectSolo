a4e08e39252c1ee3c3eaff9f64de6fea1555a0c3
nisaaja-panggil <nisa31102007@gmail.com>
2024-05-06 20:20:19 +0700

    intall laravel dan membuat database

---
+++ database/migrations/2024_05_06_123114_create_pinjam_bukus_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pinjam_bukus', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('book_id')->unsigned();
+            $table->bigInteger('anggota_id')->unsigned();
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pinjam_bukus');
+    }
+};
