a052bf0602517787869065580171236fb449ee12
nisaaja-panggil <nisa31102007@gmail.com>
2024-05-30 10:09:44 +0700

    merubah sedikit , agar tekan perpustakaan langsung diarah kan ke koleksi buku

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -69,7 +69,7 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
+            <a href="{{route('book.index')}}" class="brand-link">
                 <img src="{{ asset('dist/img/pngwing.com(1).png')}}" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light"><B>PERPUSTAKAAN<B></span>
